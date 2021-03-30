a98f8d2181630a8fe614d5fe717e096a9dd49af2
amenchaca100 <menchacanton@gmail.com>
2021-03-30 11:11:26 -0400

    setup files

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+let app = express();
+// var router = require('./routes.js');
+
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static(__dirname + '/../dist'));
+var PORT = 3000;
+
+
+app.listen(PORT, function() {
+  console.log(`listening on port ${PORT}`);
+});
